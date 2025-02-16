ce178998f5f600d85f5712d1688004d4a140f4ec
jasminSlootweg <105336332+jasminSlootweg@users.noreply.github.com>
2025-02-16 02:46:01 -0500

    Added story book functionality

--- Assets/Book-Page Curl/scripts/AutoFlip.cs
+++ Assets/Book-Page Curl/scripts/AutoFlip.cs
@@ -123,6 +123,3 @@ public class AutoFlip : MonoBehaviour {
         ControledBook.ReleasePage();
     }
 }
-//The implementation is based on this article:http://rbarraza.com/html5-canvas-pageflip/
-//As the rbarraza.com website is not live anymore you can get an archived version from web archive 
-//or check an archived version that I uploaded on my website: https://dandarawy.com/html5-canvas-pageflip/
